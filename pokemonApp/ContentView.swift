d3561582f390af979d9b0860d9421149ded1d7f6
yutokunaga <yutokunaga4107@gmail.com>
2024-12-04 16:53:50 +0900

    Initial Commit

---
+++ pokemonApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pokemonApp
+//
+//  Created by Yu Tokunaga on 2024/12/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
