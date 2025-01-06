7586a37c022e0870b3a91c1926645a6dcca86e69
yutokunaga <yutokunaga4107@gmail.com>
2025-01-06 20:06:48 +0900

    Initial commit with PokemonApp files

--- pokemonApp/ContentView.swift
+++ pokemonApp/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+        ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView{
+            LazyVGrid(columns: columns, spacing: 10){
+                ForEach(0...150, id: \.self) { index in
+                    ZStack {
+                        
+                        //背景白
+                        Color.white
+                            .ignoresSafeArea()
+                        // 上半分（赤）
+                        Circle()
+                            .fill(Color.red)
+                            .frame(width: 200, height: 200)
+                        
+                        // 下半分（白）
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: 200, height: 200)
+                            .clipShape(Rectangle().offset(y: 100)) // 下半分のみ表示
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.black, lineWidth: 4) // 外枠を追加
+                            )
+                            .overlay(
+                                //テキスト
+                                Text("\(index + 1)")
+                                    .multilineTextAlignment(.center)
+                                    .foregroundColor(.black)
+                            )
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
